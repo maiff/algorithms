9fe42ab74fc66723249cdc327c511936b7ce89e8
xwt向王涛 <wtxiang@ctrip.com>
2017-06-16 13:38:28 +0800

    bank up

--- boyer-moore/bm.js
+++ boyer-moore/bm.js
@@ -1,105 +1,110 @@
-const ERROR = 0
-const TRUE  = 1
-//获取“坏字符”移动距离
-function BadValue(bad, target){
-    let i =0
-    let badindex = -1;
-    for(i = 0; i < target.length; i++){
-        if(bad==target[i]){
-            badindex = target.length - 1 - i;
-        }
-    }
-    return badindex;
-}
+var BadValue, GetValue, BM, printf;
+(function () {
+    var _$0 = this;
 
-//获取value“后缀规则”，移动距离
-function GetValue(value, target){
-    let i = 0,j = 0;
-    let temp;
-    let tarlen = target.length;
-    value = [];
+    function _0(bad, target) {
+        let i = 0;
+        let badindex = -1;
 
-    for(i = 0; i < tarlen; i++){  //初始化
-        value[i] = -1;    //-1代表后缀无匹配的字符
-    }
-    for(i = tarlen -1; i >0; i--){
-        temp = target[i];
-
-        if(i <tarlen -1) 
-            value[i] = value[i+1];  //后一位赋值到前一位
-
-        for(j = 0; j<i ;j++){
-            if(target[j] ==temp){
-                value[i] = i - j;   //移动的距离
+        for (i = 0; i < target.length; i++) {
+            if (bad == target[i]) {
+                badindex = target.length - 1 - i;
             }
         }
-    }
-    return value;
-}
 
-function BM(source, target, value){
-    let i = 0,j = 0, soulen = source.length,tarlen = target.length;  //初始化
-    let badvalue = 0,distance = 0;
-    if(soulen < tarlen){  //比较长度
-        printf("字符串的长度小于搜索词的长度\n");
-        return ERROR;
+        return badindex;
     }
 
-    i = tarlen -1;j = tarlen -1; //从尾开始匹配
+    function _1(value, target) {
+        let i = 0,
+            j = 0;
+        let temp;
+        let tarlen = target.length;
+        value = [];
 
-    while(i < soulen ){
-        if(source[i] == target[j]){  //字符匹配成功
-            if(j ==0){
-                printf("匹配成功\n");
-                return i;
-            }
+        for (i = 0; i < tarlen; i++) {
+            //初始化
+            value[i] = -1; //-1代表后缀无匹配的字符
+        }
 
-            i--;j--;
-        }else{  //未匹配成功
-            if(j == tarlen -1){  //如果尾字符未匹配成功，"坏字符规则"
-                badvalue = BadValue(source[i],target);
-                if(badvalue == -1)
-                    badvalue = target.length;
+        for (i = tarlen - 1; i > 0; i--) {
+            temp = target[i];
+            if (i < tarlen - 1) value[i] = value[i + 1]; //后一位赋值到前一位
 
-                i =i+ tarlen - 1 - j + badvalue;
-                j = tarlen -1;
-            }else{  //有后缀,比较"坏字符规则"和"后缀规则"
-                badvalue = BadValue(source[i],target);
-                if(badvalue == -1)
-                    badvalue = target.length;
-                distance = badvalue > value[j] ? badvalue : value[j];
-                
-                // printf(`移动距离:%d\n`,distance);
-                j = tarlen -1;   //更新j位置
-                i = i+ tarlen - 1 - j + distance;  //更新i的位置
+            for (j = 0; j < i; j++) {
+                if (target[j] == temp) {
+                    value[i] = i - j; //移动的距离
+                }
             }
         }
+
+        return value;
     }
-    printf("匹配失败....");
-    return -1;
-}
 
-function printf (...str) {
-  console.log(...str)
-}
+    function _2(source, target, value) {
+        let i = 0,
+            j = 0,
+            soulen = source.length,
+            tarlen = target.length; //初始化
 
+        let badvalue = 0,
+            distance = 0;
 
+        if (soulen < tarlen) {
+            //比较长度
+            _$0.printf("字符串的长度小于搜索词的长度\n");
 
-const fs = require('fs')
-let r = fs.readFileSync('./test.txt')
-// console.log(r.toString())
-let source=r.toString(),target='fsdfsdffsc';  //source字符串，target搜索词
-let value = [];  //好后缀词表
+            return _3;
+        }
 
-let i = 0;
-//InitData(&source,&target);  //初始化
+        i = tarlen - 1;
+        j = tarlen - 1; //从尾开始匹配
 
-value = GetValue(value,target); //获取后缀表
+        while (i < soulen) {
+            if (source[i] == target[j]) {
+                //字符匹配成功
+                if (j == 0) {
+                    _$0.printf("匹配成功\n");
 
-console.time('bm')
-BM(source,target,value)
-console.timeEnd('bm')
-console.time('indexof')
-source.indexOf(target)
-console.timeEnd('indexof')
-// printf(BM(source,target,value)===source.indexOf(target));  //BM算法
+                    return i;
+                }
+
+                i--;
+                j--;
+            } else {
+                //未匹配成功
+                if (j == tarlen - 1) {
+                    //如果尾字符未匹配成功，"坏字符规则"
+                    badvalue = _$0.BadValue(source[i], target);
+                    if (badvalue == -1) badvalue = target.length;
+                    i = i + tarlen - 1 - j + badvalue;
+                    j = tarlen - 1;
+                } else {
+                    //有后缀,比较"坏字符规则"和"后缀规则"
+                    badvalue = _$0.BadValue(source[i], target);
+                    if (badvalue == -1) badvalue = target.length;
+                    distance = badvalue > value[j] ? badvalue : value[j]; // printf(`移动距离:%d\n`,distance);
+
+                    j = tarlen - 1; //更新j位置
+
+                    i = i + tarlen - 1 - j + distance; //更新i的位置
+                }
+            }
+        }
+
+        _$0.printf("匹配失败....");
+
+        return -1;
+    }
+
+    function _4(str) {
+        _$0.console.log(str);
+    }
+
+    BadValue = _0;
+    GetValue = _1;
+    let _3 = 0;
+    BM = _2;
+    printf = _4;
+    console.log("\u5339\u914D\u5931\u8D25....");
+}).call(this);
